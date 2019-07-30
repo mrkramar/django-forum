537ba28d6d5c1290a7c6bfe9c24391ffd5fea597
Maros Kramar <maroskramar2@gmail.com>
2019-07-30 22:11:51 +0200

    Added some basic concepts

--- app/forum/views.py
+++ app/forum/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
-
 def index(request):
-    context = {
 
+    topics = [
+        {
+            'title': 'Cars',
+            'description': 'Discuss about your favorite cars here.'
+        },
+        {
+            'title': 'Design',
+            'description': 'Post your ideas.'
+        }
+    ]
+
+    context = {
+        'topics': topics
     }
+
     return render(request, 'forum/index.html', context)
