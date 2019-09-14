60915a6384336d87cb1e34333637529b756acedc
Maros Kramar <maroskramar2@gmail.com>
2019-09-14 19:54:03 +0200

    Posts can now be listed under corresponding topics, created and edited

--- app/forum/urls.py
+++ app/forum/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import (
+    TopicListView,
+    TopicDetailView,
+    PostCreateView,
+    PostDetailView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 
 urlpatterns = [
-    path('', views.index, name='forum-index')
+    path('', TopicListView.as_view(), name='forum-index'),
+    path('topic/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
+
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- app/forum/views.py
+++ app/forum/views.py
@@ -1,16 +1,76 @@
 from django.shortcuts import render
-from .models import Topic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
-"""
-Index view.
-"""
-def index(request):
+from .models import Topic, Post
 
-    context = {
-        'topics': Topic.objects.all()
-    }
+# Topic views
 
-    return render(request, 'forum/index.html', context)
+class TopicListView(ListView):
+    model = Topic
+    template_name = 'forum/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'topics'
+
+class TopicDetailView(DetailView):
+    model = Topic
+
+    def get_context_data(self, **kwargs):
+        context = super(TopicDetailView, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(topic=self.kwargs.get('pk'))
+        return context
+
+# Post views
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False    
+
+# Comment views
+
+
+
+
+# Static pages
 
 def login(request):
     return render(request, 'forum/login.html')
+
+def logout(request):
+    return render(request, 'forum/logout.html')
