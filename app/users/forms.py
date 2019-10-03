bf0e64ae314769c595e5132c65f31fc7a9d40a86
Maros Kramar <maroskramar2@gmail.com>
2019-10-03 11:20:28 +0200

    Added 'about' text field to user public profile

--- app/users/forms.py
+++ app/users/forms.py
@@ -20,11 +20,13 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
-            'username',
             'email'
         ]
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = [
+            'about',
+            'image'
+        ]
