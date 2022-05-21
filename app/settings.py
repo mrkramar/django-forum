f7a82693a84e53927b7809eadaaab9fbeeacea33
vide <krajncvide53@gmail.com>
2022-05-22 01:25:37 +0200

    adding postgres functionality to project

--- app/settings.py
+++ app/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+DATABASES = {}
+import dj_database_url
+
+DATABASES["default"] = dj_database_url.config(
+    default="postgres://postgres:postgres@127.0.0.1:5432/postgres"
+)
 
 
 # Password validation
